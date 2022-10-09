f845694363b4440911af39e35b0f01e5299088e2
Paul Kanas <paulkanas@gmail.com>
2022-10-09 18:50:01 +1030

    Button functionality

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const container = document.querySelector('#container');
 const button = document.querySelector("button");
 
-let no = 50;
+let no = 16;
 let frame = (700 - no * 2)/no;
+container.style.gridTemplateColumns = `repeat(${no}, 1fr)`;
+createGrid();
 
-for (i = 1; i <= (no*no); i++) {
-    div = container.appendChild(document.createElement('div'));
-    div.classList.add('div');
-    div.style.border = "1px solid";
-    div.style.height = `${frame}px`;
+function createGrid() {
+    for (i = 1; i <= (no*no); i++) {
+        div = container.appendChild(document.createElement('div'));
+        div.classList.add('div');
+        div.style.border = "1px solid";
+        div.style.height = `${frame}px`;
+}
 }
 
-container.style.gridTemplateColumns = `repeat(${no}, 1fr)`;
+function clearGrid() {
+    var divs = document.querySelectorAll('.div');
+    for (i = 0; i < divs.length; i++) {
+        divs[i].remove();
+    }
+}
 
-
+button.addEventListener("click", () => {
+    no = (prompt("Choose the dimensions of your grid"));
+    frame = (700 - no * 2)/no;
+    container.style.gridTemplateColumns = `repeat(${no}, 1fr)`;
+    clearGrid();
+    createGrid();
+})
